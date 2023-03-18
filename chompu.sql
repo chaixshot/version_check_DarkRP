45cf4fc7dc8cf3fbe307b8b2c211ed237fc0b670
H@mer <13221018+chaixshot@users.noreply.github.com>
2023-03-19 04:27:35 +0700

    Update chompu.sql

--- chompu.sql
+++ chompu.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `คนงาน` (
+CREATE TABLE `คนงาน` (
   `รหัสคนงาน` int(11) NOT NULL DEFAULT 0,
   `ชื่อคนงาน` char(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `อัตราค่าแรงต่อชม` int(11) DEFAULT NULL,
@@ -7,11 +7,11 @@ CREATE TABLE IF NOT EXISTS `คนงาน` (
   PRIMARY KEY (`รหัสคนงาน`),
   KEY `FK_คนงาน_ความชำนาน` (`ประเภทความชำนาน`) USING BTREE,
   CONSTRAINT `FK_คนงาน_ความชำนาน` FOREIGN KEY (`ประเภทความชำนาน`) REFERENCES `ความชำนาน` (`ประเภทความชำนาน`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+);
 
-CREATE TABLE IF NOT EXISTS `ความชำนาน` (
+CREATE TABLE`ความชำนาน` (
   `ประเภทความชำนาน` char(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `อัตราโบนัส` int(11) DEFAULT NULL,
   `จำนวนชั่วโมงขั้นต่ำ` int(11) DEFAULT NULL,
   KEY `ประเภทความชำนาน` (`ประเภทความชำนาน`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+);
